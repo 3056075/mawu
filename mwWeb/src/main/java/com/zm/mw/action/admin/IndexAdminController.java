8daa00d6525b5d659ae53d8e0acfa8182e1af2df
mm <mm@mmpc>
2015-04-03 21:42:59 +0800

    1

--- mwWeb/src/main/java/com/zm/mw/action/admin/IndexAdminController.java
+++ mwWeb/src/main/java/com/zm/mw/action/admin/IndexAdminController.java
@@ -3,11 +3,9 @@ package com.zm.mw.action.admin;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.zm.common.action.BaseController;
-
 @Controller
 @RequestMapping("/admin")
-public class IndexController extends BaseController {
+public class IndexAdminController extends BaseAdminController {
 	@RequestMapping("index")
 	public String index() {
 		return "admin/index";
